b387dd60c8bdf35c853f2b690d4c22a04d1459eb
Pool Ortega <poolortegas@gmail.com>
2023-03-17 08:37:46 -0600

    Set active note

--- src/components/SideBarItem.tsx
+++ src/components/SideBarItem.tsx
@@ -1,12 +1,19 @@
 import { useMemo } from "react";
+import { useAppDispatch } from "../hooks/useAppDispatch";
+import { setActiveNote } from "../store/journal";
 import { Note } from "../types/Notes";
 
-export default function SideBarItem({ title, body }: Note) {
+export default function SideBarItem({ title, body, date, id, imageUrls = [] }: Note) {
     const newTitle = useMemo(() => {
         return title.length > 17 ? title.substring(0, 17) + '...' : title;
     }, [title])
+
+    const dispatch = useAppDispatch()
+    const handleSelectedNote = () => {
+        dispatch(setActiveNote({ title, body, date, id, imageUrls }))
+    }
     return (
-        <div >
+        <div onClick={handleSelectedNote}>
             <h2>{newTitle}</h2>
             <p>{body}</p>
         </div>
--- src/types/Notes.ts
+++ src/types/Notes.ts
@@ -4,5 +4,6 @@ export interface Note {
     title: string,
     body: string,
     date: number
+    imageUrls?: string[]
 }
 
